6a6e36f7dab5599ffc5bdf8f49ce036db5182479
ifas-123 <ifas80885@gmail.com>
2026-09-04 20:18:27 +0530

    fix: configure database connection for CI

--- src/repository/DatabaseConnection.java
+++ src/repository/DatabaseConnection.java
@@ -6,16 +6,51 @@ import java.sql.SQLException;
 
 public class DatabaseConnection {
 
-    private static final String URL =
-            "jdbc:mysql://localhost:3306/sunrise_dental_clinic";
+    private static final String HOST =
+            System.getProperty(
+                    "db.host",
+                    "localhost"
+            );
+
+    private static final String PORT =
+            System.getProperty(
+                    "db.port",
+                    "3306"
+            );
+
+    private static final String DATABASE =
+            System.getProperty(
+                    "db.name",
+                    "sunrise_dental_clinic"
+            );
 
     private static final String USER =
-            "root";
+            System.getProperty(
+                    "db.user",
+                    "root"
+            );
 
     private static final String PASSWORD =
-            "";
+            System.getProperty(
+                    "db.password",
+                    ""
+            );
 
-    public static Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(URL, USER, PASSWORD);
+    private static final String URL =
+            "jdbc:mysql://"
+                    + HOST
+                    + ":"
+                    + PORT
+                    + "/"
+                    + DATABASE;
+
+    public static Connection getConnection()
+            throws SQLException {
+
+        return DriverManager.getConnection(
+                URL,
+                USER,
+                PASSWORD
+        );
     }
 }
